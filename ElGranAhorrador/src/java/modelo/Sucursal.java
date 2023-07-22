1a42e15289e2b975af5eb56b664c127cd3dfccfc
cargueta-2021622 <cargueta-2021622@kinal.edu.gt>
2023-07-21 22:23:11 -0600

    Se actualizo el modelo y modelo DAO de la entidad sucursal por Carlos Argueta 2021622

--- ElGranAhorrador/src/java/modelo/Sucursal.java
+++ ElGranAhorrador/src/java/modelo/Sucursal.java
@@ -1,65 +1,65 @@
 package modelo;
 
-import java.time.LocalTime;
+import java.sql.Date;
 
 
-public class Sucursal {
+public class Sucursal{
   private int idSucursal;
   private String nombreSucursal;
   private String direccionSucursal;
-  private LocalTime apertura;
-  private LocalTime cierre;
+  private Date apertura;
+  private Date cierre;
 
   public Sucursal() {
   }
 
-  public Sucursal(int idSucursal, String nombreSucursal, String direccionSucursal, LocalTime apertura, LocalTime cierre) {
+  public Sucursal(int idSucursal, String nombreSucursal, String direccionSucursal, Date apertura, Date cierre) {
     this.idSucursal = idSucursal;
     this.nombreSucursal = nombreSucursal;
     this.direccionSucursal = direccionSucursal;
     this.apertura = apertura;
     this.cierre = cierre;
-  }
+    }
 
-  public int getIdSucursal() {
-    return idSucursal;
-  }
+    public int getIdSucursal() {
+        return idSucursal;
+    }
 
-  public void setIdSucursal(int idSucursal) {
-    this.idSucursal = idSucursal;
-  }
+    public void setIdSucursal(int idSucursal) {
+        this.idSucursal = idSucursal;
+    }
 
-  public String getNombreSucursal() {
-    return nombreSucursal;
-  }
+    public String getNombreSucursal() {
+        return nombreSucursal;
+    }
 
-  public void setNombreSucursal(String nombreSucursal) {
-    this.nombreSucursal = nombreSucursal;
-  }
+    public void setNombreSucursal(String nombreSucursal) {
+        this.nombreSucursal = nombreSucursal;
+    }
 
-  public String getDireccionSucursal() {
-    return direccionSucursal;
-  }
+    public String getDireccionSucursal() {
+        return direccionSucursal;
+    }
 
-  public void setDireccionSucursal(String direccionSucursal) {
-    this.direccionSucursal = direccionSucursal;
-  }
+    public void setDireccionSucursal(String direccionSucursal) {
+        this.direccionSucursal = direccionSucursal;
+    }
 
-  public LocalTime getApertura() {
-    return apertura;
-  }
+    public Date getApertura() {
+        return apertura;
+    }
 
-  public void setApertura(LocalTime apertura) {
-    this.apertura = apertura;
-  }
+    public void setApertura(Date apertura) {
+        this.apertura = apertura;
+    }
 
-  public LocalTime getCierre() {
-    return cierre;
-  }
+    public Date getCierre() {
+        return cierre;
+    }
 
-  public void setCierre(LocalTime cierre) {
-    this.cierre = cierre;
-  }
-  
-  
+    public void setCierre(Date cierre) {
+        this.cierre = cierre;
+    }
+
+   
 }
--- ElGranAhorrador/src/java/modelo/SucursalDAO.java
+++ ElGranAhorrador/src/java/modelo/SucursalDAO.java
@@ -4,7 +4,8 @@ import config.Conexion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SucursalDAO {
@@ -26,8 +27,7 @@ public class SucursalDAO {
         sucursal.setIdSucursal(rs.getInt("idSucursal"));
         sucursal.setNombreSucursal(rs.getString("nombreSucursal"));
         sucursal.setDireccionSucursal(rs.getString("direccionSucursal"));
-        //sucursal.setApertura(rs.getLocalTime("apertura"));
-        //LocalTime.parse(sucursal.setCierre(rs.getTime("cierre")));
+        
       }
       
     }catch(Exception e){
@@ -37,22 +37,108 @@ public class SucursalDAO {
     return sucursal;
    }
   
+  //Listar Sucursal
+  
+  public List listar(){
+    String sql = "select * from Sucursal";
+    List<Sucursal> listaSucursal = new ArrayList<>();
+    try{
+      con = cn.Conexion();
+      ps = con.prepareStatement(sql);
+      rs = ps.executeQuery();
+      while (rs.next()){
+        Sucursal su = new Sucursal();
+        su.setIdSucursal(rs.getInt(1));
+        su.setNombreSucursal(rs.getString(2));
+        su.setDireccionSucursal(rs.getString(3));
+        
+        listaSucursal.add(su);
+      }
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+    
+    return listaSucursal;
+  }
+  
+  
   //Agregar Sucursal
   
+  public int agregar(Sucursal sucur){
+    String sql = "insert into Sucursal (nombreSucursal, direccionSucursal, apertura, cierre) values (?,?,?,?)";
+    try{
+      con = cn.Conexion();
+      ps = con.prepareStatement(sql);
+      ps.setString(1, sucur.getNombreSucursal());
+      ps.setString(2, sucur.getDireccionSucursal());
+      
+      ps.executeUpdate();
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+    
+    return resp;
+  }
   
   
+  //Buscar por código Sucursal
+  
+  public Sucursal listarIdSucursal(int id){
+    //instanciar un objeto de tipo Empleado
+    Sucursal sucur = new Sucursal();
+    String sql = "Select * from Sucursal where idSucursal = "+id;
+    try{
+      con = cn.Conexion();
+      ps = con.prepareStatement(sql);
+      rs = ps.executeQuery();
+      while(rs.next()){
+        sucur.setNombreSucursal(rs.getString(2));
+        sucur.setDireccionSucursal(rs.getString(3));
+        //apertura
+        //cierre
+      }
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+    
+    return sucur;
+  }
   
   
+  //Editar Sucursal
+  
+  public int actualizar(Sucursal sucur){
+    String sql = "Update Sucursal set nombreSucursal = ?, direccionSucursal = ?, apertura = ?, cierre = ? where idSucursal = ?";
+    try{
+      con = cn.Conexion();
+      ps = con.prepareStatement(sql);
+      ps.setString(1, sucur.getNombreSucursal());
+      ps.setString(2, sucur.getDireccionSucursal());
+      //apertura
+      //cierre
+      ps.setInt(5, sucur.getIdSucursal());
+      ps.executeUpdate();
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+    
+    return resp;
+  }
   
   
+  //Método Eliminar
   
-  
-  
-  
-  
-  
-  
-  
+  public void eliminar(int id){
+    String sql = "delete from Sucursal where idSucursal = "+id;
+    try{
+      con = cn.Conexion();
+      ps = con.prepareStatement(sql);
+      ps.executeUpdate();
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+    
+  }
   
   
 }
